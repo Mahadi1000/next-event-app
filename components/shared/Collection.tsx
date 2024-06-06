5d7428e9062de2e5f9b55ff70096cf76dd4e6883
Mahadi Hasan <muhammadmahdihasan1@gmail.com>
2024-06-06 17:06:51 +0600

    error sovled

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -1,23 +1,6 @@
 import React from 'react'
-type CollectionProps = {
-  data: IEvent[],
-  emptyTitle: string,
-  emptyStateSubtext: string,
-  limit: number,
-  page: number | string,
-  totalPages?: number,
-  urlParamName?: string,
-  collectionType?: 'Events_Organized' | 'My_Tickets' | 'All_Events'
-}
-const Collection = ({
-  data,
-  emptyTitle,
-  emptyStateSubtext,
-  page,
-  totalPages = 0,
-  collectionType,
-  urlParamName,
-}: CollectionProps)) => {
+
+const Collection = () => {
   return (
     <div>Collection</div>
   )
